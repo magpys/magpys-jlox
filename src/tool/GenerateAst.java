132f3185c57122e5c13ddaebd0701c0ce0b2a973
Magpys <1dangriffiths@gmail.com>
2025-08-14 07:46:27 +0100

    Up to chapter 9 challenges

--- src/tool/GenerateAst.java
+++ src/tool/GenerateAst.java
@@ -19,6 +19,7 @@ public class GenerateAst {
                 "Binary: Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
                 "Literal: Object value",
+                "Logical: Expr left, Token operator, Expr right",
                 "Unary: Token operator, Expr right",
                 "Variable: Token name"
         ));
@@ -26,7 +27,9 @@ public class GenerateAst {
         defineAst(outputDir, "Stmt", Arrays.asList(
                 "Block : List<Stmt> statements",
                 "Expression : Expr expression",
+                "If : Expr condition, Stmt thenBranch, Stmt elseBranch",
                 "Print : Expr expression",
+                "While : Expr condition, Stmt body",
                 "Var : Token name, Expr initializer"
         ));
     }
